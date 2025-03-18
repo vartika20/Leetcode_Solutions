917f1d5f023539ea1fa47c7d0de1cb8f58b1c0b8
vartika20 <66714903+vartika20@users.noreply.github.com>
2025-03-18 23:54:25 +0530

    Time: 2 ms (41.62%), Space: 55.3 MB (29.69%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if(s.length > t.length) return false;
+
+    let j=0;
+    for(let i=0;i<t.length;i++){
+        if(s[j] === t[i]){
+            j++;
+        }
+    }
+   return j===s.length;
+};
